51d36cb6304d5ffd57c65682e67677d1115161d0
kerwinn447 <144878917+kerwinn447@users.noreply.github.com>
2023-09-27 17:29:53 +0800

    Add files via upload

--- .R
+++ .R
@@ -1,3 +1,16 @@
 c("John", "Mary")
 c(2, 3.1412)
 c(TRUE, TRUE, F)
+list("John", 178, TRUE)
+typeof(c("John", "Mary"))
+typeof(list("John", 178, TRUE))
+"John"
+5
+TRUE
+list("John", 5, TRUE)
+c(5, 7, 9)
+list("John", 5, TRUE) # has 3 element values.
+list("John",42, list("Mary", 35)) # big list (a nested list)
+"John"
+42
+list("Mary", 35)
